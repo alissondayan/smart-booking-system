50773bb47cb945cfa7f3752250fe2e6fa1a2bada
alisson dayan <alissondayan@h-MacBook-Pro-sl-alisson.local>
2026-06-23 19:24:01 +0300

    Stable backup before next changes

--- backend/src/modules/health/health.module.ts
+++ backend/src/modules/health/health.module.ts
@@ -1,9 +1,4 @@
 import { Module } from '@nestjs/common';
-import { HealthController } from './presentation/health.controller';
-import { HealthService } from './health.service';
 
-@Module({
-  controllers: [HealthController],
-  providers: [HealthService],
-})
+@Module({})
 export class HealthModule {}
